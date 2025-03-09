2a6e6e18aa56096d37a0075a422d161c9bc3032d
Lachlan Dow <lachlan.dow@stcatz.ox.ac.uk>
2025-03-09 17:04:39 +0000

    Update array_delete.hpp

--- part-1/array_delete.hpp
+++ part-1/array_delete.hpp
@@ -8,7 +8,11 @@
 template <typename T>
 void array_delete(std::vector<T> &A, std::size_t index)
 {
-    // WRITE YOUR CODE HERE
+    vector<T>:: iterator it
+    for (it = index; it = size(A) - 1; it++){
+        *it = *(it + 1)
+            }
+    A.resize(size(A) - 1)
 }
 
 #endif // __array_delete__
